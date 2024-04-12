4ec8beb6910fe435b16c6921810ec074b6eb37f2
Adelia B <adelia.baudn@gmail.com>
2024-04-12 12:02:05 +0200

    Initial commit

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil Fournil alsacien</title>
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <a class="headerElem" href="index.php">Le fournil alsacien</a>
+        <div class="headerElem">Nos produits : </div>
+        <div class="headerElem">
+            <a href="">Pains</a>
+            <a href="">Viennoiseries</a>
+            <a href="">Spécialités</a>
+        </div>
+        <a class="headerElem" href="">Commandes</a></p>
+    </header>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+header('Content-type: text/html; charset=utf-8');
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil Fournil alsacien</title>
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+    <section>
+        <?php
+        include("header.php");
+        ?>
+        Page d'accueil
+
+    </section>
+</body>
+</html>
