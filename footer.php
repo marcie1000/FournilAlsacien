ff4a249e67d919a39e926d13e77987db1e052298
Aymée Jaquemyns <aymee.jaquemyns@gmail.com>
2024-04-17 14:10:11 +0200

    Ajout footer

---
+++ footer.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil Fournil alsacien</title>
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+    <footer>
+    <a class="footerElem" href="">Mentions légales</a></p>
+    <a class="footerElem" href="">Nous contacter</a></p>
+</footer>
+</body>
+</html>
