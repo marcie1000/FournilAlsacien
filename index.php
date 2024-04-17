b04374561250427a972b862ab56bcbc3a33300b9
Adelia B <adelia.baudn@gmail.com>
2024-04-17 14:47:51 +0200

    Mise en forme
    Modification du css pour afficher le header et footer en haut et en bas
    de l'écran, modification de la police d'écriture

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ header('Content-type: text/html; charset=utf-8');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil Fournil alsacien</title>
+    <?php include("fonts.php");?>
     <link href="style.css" rel="stylesheet">
 </head>
 <body>
